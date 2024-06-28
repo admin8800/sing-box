5c38b2f12f4dabeacf3d67c25bfeb888fe6bf605
admin8800 <153331261+admin8800@users.noreply.github.com>
2024-06-28 11:34:58 +0800

    Update init.sh

--- src/init.sh
+++ src/init.sh
@@ -18,7 +18,7 @@ _cyan() { echo -e ${cyan}$@${none}; }
 _green() { echo -e ${green}$@${none}; }
 _yellow() { echo -e ${yellow}$@${none}; }
 _magenta() { echo -e ${magenta}$@${none}; }
-_red_bg() { echo -e "\e[41m$@${none}"; }
+_red_bg() { echo -e "\e[42m$@${none}"; }
 
 _rm() {
     rm -rf "$@"
@@ -34,7 +34,7 @@ _mkdir() {
 }
 
 is_err=$(_red_bg 错误!)
-is_warn=$(_red_bg 警告!)
+is_warn=$(_red_bg 提示!)
 
 err() {
     echo -e "\n$is_err $@\n"
